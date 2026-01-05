bcadf0ca3fb325a16e98691c58d5f8f0ed5738af
James Orban <orbanjams4@gmail.com>
2026-01-05 01:49:57 +0100

    modified exc1

--- exc1/dict_count.py
+++ exc1/dict_count.py
@@ -13,4 +13,22 @@ for lin in hand:
         di[w] = di.get(w,0) + 1
         # print(w, 'new', di[w])
        
-print(di)
+# print(di) Only to see the dictionary
+
+# find the most common word
+# bigcount = None
+# bigword = None
+# for w,c in di.items():
+#     if bigcount is None or c > bigcount:
+#         bigword = w
+#         bigcount = c
+# print(bigword, bigcount)
+# OR
+largest = -1
+theword = None
+for k,v in di.items():
+    if v > largest:
+        largest = v
+        theword = k
+print(theword, largest)
+
