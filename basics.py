75cc529908e54c994a19e9c229aff16da99a561e
James Orban <orbanjams4@gmail.com>
2026-01-06 21:42:09 +0100

    web services

--- basics.py
+++ basics.py
@@ -865,4 +865,6 @@ info = json.loads(data)
 print('Name:',info['name'])
 print('Hide:',info['email']['hide'])
 
+# Service Oriented Approach: Most non-trivial web applications use services. They use services from other applications. Credit Card Charge, Hotel Reservation Systems. Services publish the "rules" applications must follow to make use of the service(API)
 
+# Application Program Interface: The API itself is largely abstract in that it specifies an interface and controls the behavior of the objects specified in that interface. The software that provides the functionality described by an API is said to be an "implementation" of the API. An API is typically defined in terms of the programming languiafge used to be build an application.
