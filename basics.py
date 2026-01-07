5b38f3ddfc3a5cfc8a404748695f975f6fd372c6
James Orban <orbanjams4@gmail.com>
2026-01-07 05:40:17 +0100

    modified files

--- basics.py
+++ basics.py
@@ -937,3 +937,20 @@ s.party()
 j = PartyAnimal('Jim')
 j.party()
 s.party()
+
+# Inheritance: When we make a new class - we can reuse an existing class and inherit all the capabilities of an existing class and then add our own little bit to make our new class. Another form of store and reuse. Write once-reuse many times. The new class (child) has all the capabilities of the old class(parent) - and then some more. eg:
+class FootballFan(PartyAnimal):
+ points = 0
+ def touchdown(self):
+   self.points = self.points + 7
+   self.party()
+   print(self.name,"points", self.points)
+
+s = PartyAnimal('Sally')
+s.party()
+
+j = FootballFan('Jim')
+j.party()
+j.touchdown()
+
+# DATABASES: Relational Databases and SQLite
