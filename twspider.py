e746f9a72a306786a41912a654a432dd9ae324a6
James Orban <orbanjams4@gmail.com>
2026-01-07 19:56:21 +0100

    updated file

--- twspider.py
+++ twspider.py
@@ -55,11 +55,11 @@ while True:
             cur.execute('UPDATE Twitter SET friends = ? WHERE name = ?',
                         (count+1, friend))
             countold = countold + 1
-#         except:
-#             cur.execute('''INSERT INTO Twitter (name, retrieved, friends)
-#                         VALUES (?, 0, 1)''', (friend, ))
-#             countnew = countnew + 1
-#     print('New accounts=', countnew, ' revisited=', countold)
-#     conn.commit()
+        except:
+            cur.execute('''INSERT INTO Twitter (name, retrieved, friends)
+                        VALUES (?, 0, 1)''', (friend, ))
+            countnew = countnew + 1
+    print('New accounts=', countnew, ' revisited=', countold)
+    conn.commit()
 
-# cur.close()
+cur.close()
