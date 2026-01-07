3b54de84b66bb963d1ad9af27b1dc9393aa05d7d
James Orban <orbanjams4@gmail.com>
2026-01-07 05:38:20 +0100

    subclass

---
+++ footballfan.py
@@ -0,0 +1,34 @@
+# An example for inheritance/ subclass(child class)
+
+class PartyAnimal:
+     x = 0
+     name = ""
+     def __init__(self,z):
+      self.name = z
+      print(self.name, 'constructed')
+
+     def party(self):
+      self.x = self.x + 1
+      print(self.name, 'party count', self.x)
+
+s = PartyAnimal('Sally')
+s.party()
+
+j = PartyAnimal('Jim')
+j.party()
+s.party()
+
+# FootballFan is a subclass which extends PartyAnimal. It has all the capabilities of PartyAnimal class and more.
+class FootballFan(PartyAnimal):
+ points = 0
+ def touchdown(self):
+   self.points = self.points + 7
+   self.party()
+   print(self.name,"points", self.points)
+
+s = PartyAnimal('Sally')
+s.party()
+
+j = FootballFan('Jim')
+j.party()
+j.touchdown()
