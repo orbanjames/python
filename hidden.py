e913cc2cf2a3a5d435673193ef7e2ab39fbfe6ce
James Orban <orbanjams4@gmail.com>
2026-01-08 22:06:51 +0100

    updated file

--- hidden.py
+++ hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "h7Lu...Ng",
-            "consumer_secret": "dNKenAC3New...mmn7Q",
-            "token_key": "10185562-eibxCp9n2...P4GEQQOSGI",
-            "token_secret": "H0ycCFemmC4wyf1...qoIpBo"}
+    return {"consumer_key": "TDXrBnVmu3Czbxm7hFE4dElhC",
+            "consumer_secret": "6td9Mvcs4DlwDRiOcZA4cT8NhQBp79AtPO0D4KBo6V9L5qSl20",
+            "token_key": "2008691773535907846-ml0rb1UWKza9Kj5PFbi257DCJgMCr5",
+            "token_secret": "DLS80vgVf7XOB79Ouk30BnpOfkcBZsxgLG9c3pmdZhg5W"}
